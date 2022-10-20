c980533e225d04f3f7f9ba32f99581dc782f4770
Peter <peteryasser80@gmail.com>
2022-10-20 18:46:50 +0200

    Print parentTree

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,13 @@ public class Main {
         Searcher searcher=new Searcher();
         SearchResult searchResult=searcher.DFS(125349678,912345678);
         System.out.println(searchResult.explored.size());
-        int r=912345678;
-        while (searchResult.parentTree.containsKey(r)){
-            r=searchResult.parentTree.get(r);
-            System.out.println(r);
+        int goal=912345678;
+        int c=0;
+        while (searchResult.parentTree.containsKey(goal)){
+            goal=searchResult.parentTree.get(goal);
+            System.out.println(goal);
+            c++;
         }
-
+        System.out.println(c);
     }
 }
